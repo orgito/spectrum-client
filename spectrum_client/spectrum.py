e8f0f5d6a81f947f289429a5e6c1519618f97e56
Justin Drew <jdrew82@users.noreply.github.com>
2020-12-05 16:02:26 -0600

    Add error handling, update to py3, f-strings

--- spectrum_client/spectrum.py
+++ spectrum_client/spectrum.py
@@ -1,3 +1,4 @@
+#!/bin/local/python3
 """specrum_client"""
 
 import os
@@ -6,9 +7,9 @@ import xml.etree.ElementTree as ET
 import requests
 from requests.auth import HTTPBasicAuth
 
-SPECTRUM_URL = os.environ.get('SPECTRUM_URL')
-SPECTRUM_USERNAME = os.environ.get('SPECTRUM_USERNAME')
-SPECTRUM_PASSWORD = os.environ.get('SPECTRUM_PASSWORD')
+SPECTRUM_URL = os.environ.get("SPECTRUM_URL")
+SPECTRUM_USERNAME = os.environ.get("SPECTRUM_USERNAME")
+SPECTRUM_PASSWORD = os.environ.get("SPECTRUM_PASSWORD")
 
 
 class SpectrumClientException(Exception):
@@ -25,10 +26,10 @@ class SpectrumClientParameterError(SpectrumClientException):
 
 class Spectrum(object):
     """A wrapper form OneClick REST API."""
-    headers = {'Content-Type': 'application/xml'}
+    headers = {"Content-Type": "application/xml"}
 
-    xml_namespace = {'ca': 'http://www.ca.com/spectrum/restful/schema/response'}
-    default_attributes = '''
+    xml_namespace = {"ca": "http://www.ca.com/spectrum/restful/schema/response"}
+    default_attributes = """
     <rs:requested-attribute id="0x129fa"/> <!-- Model Handle -->
     <rs:requested-attribute id="0x1006e"/> <!-- Model Name -->
     <rs:requested-attribute id="0x1000a"/> <!-- Condition -->
@@ -43,8 +44,8 @@ class Spectrum(object):
     <rs:requested-attribute id="0x1295d"/> <!-- isManaged-->
     <rs:requested-attribute id="0x11564"/> <!-- Notes-->
     <rs:requested-attribute id="0x12db9"/> <!-- ServiceDesk Asset ID-->
-    '''
-    models_search_template = '''<?xml version="1.0" encoding="UTF-8"?>
+    """
+    models_search_template = """<?xml version="1.0" encoding="UTF-8"?>
     <rs:model-request throttlesize="9999"
     xmlns:rs="http://www.ca.com/spectrum/restful/schema/request"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -58,9 +59,9 @@ class Spectrum(object):
                 </rs:search-criteria>
             </rs:models-search>
         </rs:target-models>
-    ''' + default_attributes + '</rs:model-request>'
+    """ + default_attributes + "</rs:model-request>"
 
-    event_by_ip_template = '''<?xml version="1.0" encoding="UTF-8"?>
+    event_by_ip_template = """<?xml version="1.0" encoding="UTF-8"?>
     <rs:event-request throttlesize="10" xmlns:rs="http://www.ca.com/spectrum/restful/schema/request" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.ca.com/spectrum/restful/schema/request ../../../xsd/Request.xsd">
         <rs:event>
             <rs:target-models>
@@ -86,36 +87,57 @@ class Spectrum(object):
             {var_binds}
         </rs:event>
     </rs:event-request>
-'''
+"""
 
     def __init__(self, url=SPECTRUM_URL, username=SPECTRUM_USERNAME, password=SPECTRUM_PASSWORD):
         if url is None:
-            raise ValueError('Spectrum (OneClick) url must be provided either in the constructor or as an environment variable')
-        self.url = url if not url.endswith('/') else url[:-1]
+            raise ValueError("Spectrum (OneClick) url must be provided either in the constructor or as an environment variable")
+        self.url = url if not url.endswith("/") else url[:-1]
         self.auth = HTTPBasicAuth(username, password)
 
     def _parse_get(self, res):
         self._check_http_response(res)
 
         root = ET.fromstring(res.content)
-        model_error = root.find('.//ca:model', self.xml_namespace).get('error')
-        if model_error:
-            raise SpectrumClientParameterError('Model Error: ' + model_error)
-        attr_error = root.find('.//ca:attribute', self.xml_namespace).get('error')
-        if attr_error:
-            raise SpectrumClientParameterError(attr_error)
+        try:
+            model_error = root.find(".//ca:model", self.xml_namespace).get("error")
+            if model_error:
+                raise SpectrumClientParameterError("Model Error: " + model_error)
+        except AttributeError:
+            pass
+
+        try:
+            attr_error = root.find(".//ca:attribute", self.xml_namespace).get("error")
+            if attr_error:
+                raise SpectrumClientParameterError(attr_error)
+        except AttributeError:
+            pass
+
+        try:
+            list_error = root.find(".//ca:attribute-list", self.xml_namespace).get("error")
+            if list_error:
+                raise SpectrumClientParameterError(list_error)
+        except AttributeError:
+            pass
+        
+        try:
+            responses_error = root.find(".//ca:model-responses", self.xml_namespace).get("error")
+            if responses_error:
+                raise SpectrumClientParameterError("Responses Error: " + responses_error)
+        except AttributeError:
+            pass
 
     def _parse_update(self, res):
         self._check_http_response(res)
 
         root = ET.fromstring(res.content)
-        if root.find('.//ca:model', self.xml_namespace).get('error') == 'Success':
+        if root.find(".//ca:model", self.xml_namespace).get("error") == "Success":
             return
 
-        if root.find('.//ca:model', self.xml_namespace).get('error') == 'PartialFailure':
-            msg = root.find('.//ca:attribute', self.xml_namespace).get('error-message')
+        if root.find(".//ca:model", self.xml_namespace).get("error") == "PartialFailure":
+            msg = root.find(".//ca:attribute", self.xml_namespace).get("error-message")
         else:
-            msg = root.find('.//ca:model', self.xml_namespace).get('error-message')
+            msg = root.find(".//ca:model", self.xml_namespace).get("error-message")
         raise SpectrumClientParameterError(msg)
 
     def _build_filter(self, filters, landscape=None):
@@ -128,36 +150,36 @@ class Spectrum(object):
                 value=f[2]
             ) for f in filters
         ]
-        filters = ['''
+        filters = ["""
             <{operation}>
                 <attribute id="{attr_id}">
                     <value>{value}</value>
                 </attribute>
-            </{operation}>'''.format(**f) for f in filters]
-        filters = '\n'.join(filters)
+            </{operation}>""".format(**f) for f in filters]
+        filters = "\n".join(filters)
         if landscape:
             landscape_filter = self.xml_landscape_filter(landscape)
         else:
-            landscape_filter = ''
+            landscape_filter = ""
 
-        models_filter = '''
+        models_filter = f"""
         <and>
             {landscape_filter}
             {filters}
-         </and>'''.format(landscape_filter=landscape_filter, filters=filters)
+         </and>"""
         return(self.models_search_template.format(models_filter=models_filter))
 
     @staticmethod
     def _check_http_response(res):
         """Validate the HTTP response"""
         if res.status_code == 401:
-            raise SpectrumClientAuthException('Authorization Failure. Invalid user name or password.')
+            raise SpectrumClientAuthException("Authorization Failure. Invalid user name or password.")
         res.raise_for_status()
 
     @staticmethod
     def xml_landscape_filter(landscape):
         """Return a xml fragment filtering by landscape"""
-        xml = '''
+        xml = """
         <greater-than>
             <attribute id="0x129fa">
                 <value>{}</value>
@@ -167,39 +189,57 @@ class Spectrum(object):
             <attribute id="0x129fa">
                 <value>{}</value>
             </attribute>
-        </less-than>'''
+        </less-than>"""
         landscape_start = hex(landscape)
         landscape_end = hex(landscape + 0xfffff)
         return xml.format(landscape_start, landscape_end).strip()
 
     def get_attribute(self, model_handle, attr_id):
         """Get an attribute from Spectrum model.
-
         Arguments:
             model_handle {int} -- Model Handle of the model being queried.
             attr_id {int} -- Attribute ID of the attribute being queried.
         """
-        url = '{}/spectrum/restful/model/{}'.format(self.url, hex(model_handle))
-        params = {'attr': hex(attr_id)}
+        if isinstance(model_handle, int):
+            model_handle = hex(model_handle)
+        if isinstance(attr_id, int):
+            attr_id = hex(attr_id)
+
+        url = f"{self.url}/spectrum/restful/model/{model_handle}"
+        params = {"attr": attr_id}
         res = requests.get(url, params=params, auth=self.auth)
         self._parse_get(res)
         root = ET.fromstring(res.content)
-        return root.find('.//ca:attribute', self.xml_namespace).text
+
+        try:
+            return root.find(".//ca:model", self.xml_namespace)
+        except:
+            pass
+
+        try:
+            return root.find(".//ca:attribute", self.xml_namespace)
+        except:
+            pass
+
+        try:
+            return root.find(".//ca:attribute-list", self.xml_namespace)
+        except:
+            pass
 
     def devices_by_filters(self, filters, landscape=None):
         """Returns a list of devices matching the filters"""
-        device_only = (0x10001, 'is-derived-from', 0x1004b)
+        device_only = (0x10001, "is-derived-from", 0x1004b)
         filters = [device_only] + filters
         xml = self._build_filter(filters, landscape)
         return self.search_models(xml)
 
     def devices_by_attr(self, attr, value, landscape=None):
         """Returns a list of devices matching an attribute value"""
-        return self.devices_by_filters([(attr, 'equals', value)], landscape)
+        return self.devices_by_filters([(attr, "equals", value)], landscape)
 
     def devices_by_name(self, regex, landscape=None):
         """Returns a list of devices for which the name matches a regex"""
-        return self.devices_by_filters([('0x1006e', 'has-pcre', regex)], landscape)
+        return self.devices_by_filters([("0x1006e", "has-pcre", regex)], landscape)
 
     def models_by_filters(self, filters, landscape=None):
         """Returns a list of models matching the filters"""
@@ -208,22 +248,22 @@ class Spectrum(object):
 
     def models_by_attr(self, attr, value, landscape=None):
         """Returns a list of models matching an attribute value"""
-        return self.models_by_filters([(attr, 'equals', value)], landscape)
+        return self.models_by_filters([(attr, "equals", value)], landscape)
 
     def models_by_name(self, regex, landscape=None):
         """Returns a list of models for which the name matches a regex"""
-        return self.models_by_filters([('0x1006e', 'has-pcre', regex)], landscape)
+        return self.models_by_filters([("0x1006e", "has-pcre", regex)], landscape)
 
     def search_models(self, xml):
         """Returns the models matching the xml search"""
-        url = '{}/spectrum/restful/models'.format(self.url)
+        url = "{}/spectrum/restful/models".format(self.url)
         res = requests.post(url, xml, headers=self.headers, auth=self.auth)
         self._check_http_response(res)
         root = ET.fromstring(res.content)
-        etmodels = root.findall('.//ca:model', self.xml_namespace)
+        etmodels = root.findall(".//ca:model", self.xml_namespace)
         models = {
-            model.get('mh'): {
-                attr.get('id'): attr.text for attr in model.getchildren()
+            model.get("mh"): {
+                attr.get("id"): attr.text for attr in model.getchildren()
             } for model in etmodels
         }
         return models
@@ -244,11 +284,11 @@ class Spectrum(object):
             updates = [updates]
         updates = [
             f(x) for x in updates for f in (
-                lambda x: ('attr', hex(x[0]) if isinstance(x[0], int) else x[0]),
-                lambda x: ('val', x[1])
+                lambda x: ("attr", hex(x[0]) if isinstance(x[0], int) else x[0]),
+                lambda x: ("val", x[1])
             )
         ]
-        url = self.url + '/spectrum/restful/model/{}'.format(model_handle)
+        url = f"{self.url}/spectrum/restful/model/{model_handle}"
         res = requests.put(url, params=updates, auth=self.auth)
         self._parse_update(res)
 
@@ -256,8 +296,8 @@ class Spectrum(object):
     def generate_event_by_ip(self, event, address, variables):
         var_binds = ""
         for key, value in variables.items():
-            var_binds += '<rs:varbind id="{}">{}</rs:varbind>'.format(key, value)
+            var_binds += f"<rs:varbind id={key}>{value}</rs:varbind>"
         xml = self.event_by_ip_template.format(event=event, address=address, var_binds=var_binds)
-        url = self.url + '/spectrum/restful/events'
+        url = self.url + "/spectrum/restful/events"
         res = requests.post(url, xml, headers=self.headers, auth=self.auth)
         return res
